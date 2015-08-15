9fb031a89b751ccec415e2616c482d4833e4e59e
Richard Möhn <richard.moehn@posteo.de>
2015-08-15 15:26:13 +0900

    Use my current version 0.1.5 of guten-tag

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [me.arrdem/guten-tag "0.1.4"]
+                 [org.clojars.rmoehn/guten-tag "0.1.5"]
                  [prismatic/schema "0.4.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :source-paths ["dev"]}})
